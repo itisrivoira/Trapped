e9644a405300413687e14dbb6727125814275bd9
Giachi03 <58552437+Giachi03@users.noreply.github.com>
2022-05-03 21:36:04 +0200

    Add files via upload

---
+++ Database/DML_Trapped.sql
@@ -0,0 +1,12 @@
+USE Trapped;
+
+INSERT INTO Domande (domanda, risp_A, risp_B, risp_C, risp_D, risp_Corr) VALUES("Quale tra questi è l’autore de “Il fanciullino”?", "Pascoli", "D'Annunzio", "Manzoni", "D'Avenia", "A");
+INSERT INTO Domande (domanda, risp_A, risp_B, risp_C, risp_D, risp_Corr) VALUES("Which one of these is not a basic function of a computer?", "Storing", "Processing", "Input", "Data reading", "D");
+INSERT INTO Domande (domanda, risp_A, risp_B, risp_C, risp_D, risp_Corr) VALUES("Quali tra questi è un sistema operativo open source?", "Windows", "MacOS", "Ubuntu", "Nessuna delle precedenti", "C");
+INSERT INTO Domande (domanda, risp_A, risp_B, risp_C, risp_D, risp_Corr) VALUES("Quali tra queste non è una fase del processo di produzione di un software?", "Progetto", "Manutenzione", "Sviluppo", "Idealizzazione", "D");
+INSERT INTO Domande (domanda, risp_A, risp_B, risp_C, risp_D, risp_Corr) VALUES("Quali tra questi linguaggi di programmazione viene utilizzato per creare deiweb services?", "React Native", "Nodejs", "HTML", "XML", "B");
+INSERT INTO Domande (domanda, risp_A, risp_B, risp_C, risp_D, risp_Corr) VALUES("Chi erano i bolscevichi?", "Democratici", "Comunisti", "Nazisti", "Fascisti", "B");
+INSERT INTO Domande (domanda, risp_A, risp_B, risp_C, risp_D, risp_Corr) VALUES("Quale tra questi linguaggi di programmazione è compilato?", "C", "Python", "JavaScript", "PHP", "A");
+INSERT INTO Domande (domanda, risp_A, risp_B, risp_C, risp_D, risp_Corr) VALUES("Quale tra questi non è una funzione sql per i database?", "DDL", "DML", "Query Language", "DFL", "D");
+INSERT INTO Domande (domanda, risp_A, risp_B, risp_C, risp_D, risp_Corr) VALUES("Quanto fa l’integrale di x^2 dx?", "2x^2", "2x", "(x^3)/3", "(x^2)/2", "C");
+INSERT INTO Domande (domanda, risp_A, risp_B, risp_C, risp_D, risp_Corr) VALUES("In un servizio DNS quale tra questi protocollo viene usato per assegnare un nome ad un indirizzo ip?", "CNAME", "A Record", "SOA", "NS Record", "B");
