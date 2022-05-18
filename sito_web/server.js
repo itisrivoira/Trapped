336530be67227b151a3b3d2170d7ae06f1f1a091
MeiChang02 <97673020+MeiChang02@users.noreply.github.com>
2022-05-18 16:28:31 +0200

    Add files via upload

--- sito_web/server.js
+++ sito_web/server.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 let express = require("express");
 let bodyParser = require('body-parser');
+const { sendFile } = require('express/lib/response');
 
 let app = express();
 
@@ -15,20 +16,26 @@ app.post("/arrivamail", (req, res) => {
 	console.log("server mail");
 
 	let mailsor = req.body.sorgente
+
 	let corpomail = req.body.corpomail
+
 
 	let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: "mei.chang060802@gmail.com",
-            pass: "123"
-        }
+            user: "trapped5c@gmail.com",
+            pass: "trapped123"
+        },
+		tls:{
+			rejectUnauthorized: false,
+		},
 	});
+	
   
 	let message = {
 		from: mailsor,
-		to: "mei.chang@denina.it",
-		subject: "contacts",
+		to: "trapped5c@gmail.com",
+		subject: mailsor,
 		html: corpomail
 	}
 	
@@ -37,11 +44,12 @@ app.post("/arrivamail", (req, res) => {
 		console.log(err);
 	} else {
 		console.log(info);
+		console.log(mailsor);
+		console.log(corpomail);
 	}
 	})
 
-
-
+	
 
 });
   
