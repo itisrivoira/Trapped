eaa92911a38d0c8fbbe55ae6dc5ccdbc9dfc3de6
chiarabossolasco <chiarabossolasco003@gmail.com>
2022-05-25 21:38:56 +0200

    modifiche py

---
+++ Python/Python/varGlobali.py
@@ -0,0 +1,33 @@
+import pygame
+
+img_u_1 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_davanti_1.png')
+img_u_2 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_davanti_2.png')
+img_u_2 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_davanti_3.png')
+img_u_4 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_destra_1.png')
+img_u_5 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_destra_2.png')
+img_u_6 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_destra_3.png')
+img_u_7 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_dietro_1.png')
+img_u_8 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_dietro_2.png')
+img_u_9 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_dietro_3.png')
+img_u_10 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_sinistra_1.png')
+img_u_11 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_sinistra_2.png')
+img_u_12 = pygame.image.load('./IMMAGINI/PERSONAGGI/UOMO/uomo_sinistra_3.png')
+
+img_d_1 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_davanti_1.png')
+img_d_2 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_davanti_2.png')
+img_d_3 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_davanti_3.png')
+img_d_4 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_destra_1.png')
+img_d_5 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_destra_2.png')
+img_d_6 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_destra_3.png')
+img_d_7 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_dietro_1.png')
+img_d_8 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_dietro_2.png')
+img_d_9 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_dietro_3.png')
+img_d_10 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_sinistra_1.png')
+img_d_11 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_sinistra_2.png')
+img_d_12 = pygame.image.load('./IMMAGINI/PERSONAGGI/DONNA/donna_sinistra_3.png')
+
+character = 0
+
+flag_corretta = False
+
+vite = 3
